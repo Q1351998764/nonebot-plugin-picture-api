cd35cc77dfcb66f6c44caa3a9293cf2206a01a44
子不言 <57926506+Q1351998764@users.noreply.github.com>
2023-09-19 23:15:02 +0800

    Update config.py

--- nonebot_plugin_picture_api/config.py
+++ nonebot_plugin_picture_api/config.py
@@ -4,7 +4,6 @@ import yaml
 import nonebot
 from pathlib import Path
 
-
 config_path = Path('config/picture_api_config.yml')
 
 CONFIG_TEMPLATE = {
@@ -36,6 +35,8 @@ CONFIG_TEMPLATE = {
     {'url': 'https://xiaobapi.top/api/xb/api/meizi.php'}
     ]
 }
+
+global_config = nonebot.get_driver().config
 # 检查config文件夹是否存在 不存在则创建
 if not Path("config").exists():
     Path("config").mkdir()
@@ -43,11 +44,10 @@ if not config_path.exists():
     with open(config_path, 'w', encoding='utf-8') as f:
         yaml.dump(CONFIG_TEMPLATE, f, allow_unicode=True)  
 
-
 with open(config_path,'r') as f:
     api_data = yaml.load(f,Loader=yaml.FullLoader)#读取yaml文件
 
 class Config(BaseModel, extra=Extra.ignore):
     """Plugin Config Here"""
     api_data = api_data
-    proxies_http: str = None
+    global_config = global_config
